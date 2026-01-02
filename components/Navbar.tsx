02701389488e899476b7f782b0e64bdae5851b2d
Dominic Snyder <domrsnyder@outlook.com>
2026-01-01 23:02:17 -0600

    Implement Database Models

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from "next/dist/client/link";
-import {Image} from "next/dist/client/image-component";
+import Link from "next/link";
+import Image from "next/image";
 import posthog from 'posthog-js';
 
 const Navbar = () => {
